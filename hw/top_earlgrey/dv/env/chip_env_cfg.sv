782e71420476c2fc788b3ae10f1d4a7434320ce3
Raphael Roth <rroth@lowrisc.org>
2026-07-27 08:18:13 +0200

    [Temp] Proposed changes to the keys
    Signed-off-by: Raphael Roth <rroth@lowrisc.org>

--- hw/top_earlgrey/dv/env/chip_env_cfg.sv
+++ hw/top_earlgrey/dv/env/chip_env_cfg.sv
@@ -444,12 +444,12 @@ class chip_env_cfg #(type RAL_T = chip_ral_pkg::chip_reg_block) extends cip_base
           // suffix to the image name.
           if ("signed" inside {sw_image_flags[i]}) begin
             // Options match DEFAULT_SIGNING_KEYS in `rules/opentitan/keyutils.bzl`.
-            if ("fake_ecdsa_dev_key_0" inside {sw_image_flags[i]}) begin
-              sw_images[i] = $sformatf("%0s.fake_ecdsa_dev_key_0.signed", sw_images[i]);
-            end else if ("fake_ecdsa_prod_key_0" inside {sw_image_flags[i]}) begin
-              sw_images[i] = $sformatf("%0s.fake_ecdsa_prod_key_0.signed", sw_images[i]);
-            end else if ("fake_ecdsa_test_key_0" inside {sw_image_flags[i]}) begin
-              sw_images[i] = $sformatf("%0s.fake_ecdsa_test_key_0.signed", sw_images[i]);
+            if ("dev_key_0" inside {sw_image_flags[i]}) begin
+              sw_images[i] = $sformatf("%0s.dev_key_0.signed", sw_images[i]);
+            end else if ("prod_key_0" inside {sw_image_flags[i]}) begin
+              sw_images[i] = $sformatf("%0s.prod_key_0.signed", sw_images[i]);
+            end else if ("test_key_0" inside {sw_image_flags[i]}) begin
+              sw_images[i] = $sformatf("%0s.test_key_0.signed", sw_images[i]);
             end else if ("fake_rsa_dev_key_0" inside {sw_image_flags[i]}) begin
               sw_images[i] = $sformatf("%0s.fake_rsa_dev_key_0.signed", sw_images[i]);
             end else if ("fake_rsa_prod_key_0" inside {sw_image_flags[i]}) begin
